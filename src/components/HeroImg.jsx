c2dbc067a3b127a4f4645f7ce8f2a07e34f35e65
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-10 20:30:35 +0200

    Adds HeroImg component

---
+++ src/components/HeroImg.jsx
@@ -0,0 +1,7 @@
+import heroImg from '../assets/macbook.avif'
+
+export default function HeroImg () {
+  return (
+    <img src={heroImg} alt="Random Hero Image" className='h-screen max-w-[1440px] w-full object-cover mx-auto' />
+  )
+}
