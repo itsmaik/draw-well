774a750dfca6582b45e7d179371036477fab9a80
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-10 10:05:57 +0200

    adds logo and placeholder for dataTest

---
+++ src/data/dataTest.jsx
@@ -0,0 +1,10 @@
+const placeholder = ('https://placehold.co/300');
+
+export const CARD_CONTENT = [
+  {
+    image: placeholder,
+    title: 'Title',
+    description:
+      'Small description',
+  },
+];
