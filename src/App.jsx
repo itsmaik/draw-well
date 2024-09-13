a380644a075e5266b236e58a1f90f0628a9c4ffe
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-13 16:29:57 +0200

    Add routs to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Layout from "./layout/Layout";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
