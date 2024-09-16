c2a4fcec268ce620a4a4fae7bf12b903c5bcbb6a
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-16 11:46:56 +0200

    Removes margin-0 and d-flex from body

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -2,6 +2,6 @@ import heroImg from '../assets/macbook.avif'
 
 export default function HeroImg () {
   return (
-    <img src={heroImg} alt="Random Hero Image" className='h-screen max-w-[1440px] w-full object-cover mx-auto' />
+    <img src={heroImg} alt="Hero Image" className='h-screen max-w-[1440px] w-full object-cover mx-auto' />
   )
 }
