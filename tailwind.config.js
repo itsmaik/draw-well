6e80ecd30dd1119f908fb979c45c2be36cc07ca9
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-19 11:54:42 +0200

    Temporary solution for TW hold screen up to 1440px

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': '',
+      },  
+    },
   },
   plugins: [],
 }
