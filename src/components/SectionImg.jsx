f75f1f61e7896b471c3122dbd6656ad7fd4c80fd
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-21 21:11:19 +0200

    Adds minor updates

--- src/components/SectionImg.jsx
+++
@@ -1,7 +0,0 @@
-import heroImg from '../assets/airpods.avif'
-
-export default function HeroImg () {
-  return (
-    <img src={heroImg} alt="Hero Image" className='h-96 max-w-[1280px] w-full  object-cover mx-auto' />
-  )
-}
