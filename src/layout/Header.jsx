4241ae4ad1a811697a9fb11b73390424bd477318
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-21 23:16:30 +0200

    minor change

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import HeroImg from "../components/HeroImg"; // Assuming you have this component
+import HeroImg from "../components/HeroImg";
 
 export default function Header() {
   const location = useLocation();
