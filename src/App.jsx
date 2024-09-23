30062a867ef34a2de3de618bf0978bb83c6cdfaa
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-23 19:25:15 +0200

    Adds ProductContext and provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./layout/Layout";
+import Providers from "./context/ProductProvider";
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Providers>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
+    </Providers>
   );
 }
 
---
+++ src/context/ProductProvider.jsx
@@ -0,0 +1,36 @@
+import { useEffect, createContext, useState } from "react";
+import useFetch from "../hooks/useFetch";
+
+export const ProductContext = createContext();
+
+
+export function ProductProvider({ children }) {
+
+  const { data: products, loading, error } = useFetch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (!products) return; // Wait until products are loaded
+    if (searchQuery === '') {
+      setSearchResults(products); // If no search query, show all products
+    } else {
+      setSearchResults(
+        products.filter((product) =>
+          product.title.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    }
+  }, [searchQuery, products]);
+
+  return (
+    <ProductContext.Provider value={{ products, searchResults, loading, error, setSearchQuery }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
+
+
+export default function Providers({ children }) {
+  return <ProductProvider>{children}</ProductProvider>;
+}
