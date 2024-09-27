3f004d28dccfa26d24332f0aba13348d98862258
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-27 22:38:28 +0200

    Updates shopping cart to show the amount of products

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,12 +1,21 @@
 import { FaShoppingCart } from "react-icons/fa";
+import { useContext } from "react";
+import { ProductContext } from "../context/ProductProvider";
 
-export default function ShoppingCart() {
+
+export default function ShoppingCart({ onClick }) {
+  const { cartItems } = useContext(ProductContext);
+
+  const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
-    <button aria-label="Shopping Cart" className="relative flex items-center">
+    <button aria-label="Shopping Cart" className="relative flex items-center" onClick={onClick}>
       <FaShoppingCart className="text-gray-600" />
-      <span className="absolute -top-3 -right-5 bg-orange-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">
-        0
-      </span>
+      {totalItems > 0 && (
+        <span className="absolute -top-3 -right-5 bg-orange-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">
+          {totalItems}
+        </span>
+      )}
     </button>
-  )
+  );
 }
