ff8f529505ba7c6409ef76b9a83122586d0bb645
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-09-27 22:35:29 +0200

    Updates navbar to control sidebar modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,16 @@ import Logo from './Logo'
 import SearchBar from "./SearchBar";
 import Favorites from "./Favorites";
 import ShoppingCart from './ShoppingCart';
+import { useState } from 'react';
+import CartSidebar from './CartSideBar';
+
 
 function Navbar() {
+
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const toggleCart = () => setIsCartOpen(!isCartOpen);
+
   return (
     <nav className="bg-white p-4 shadow-md fixed top-0 w-[1440px] z-50">
       <div className="container mx-auto flex items-center justify-between">
@@ -12,8 +20,9 @@ function Navbar() {
 
         <div className="flex items-center space-x-6">
           <Favorites />
-          <ShoppingCart />
+          <ShoppingCart onClick={toggleCart}/>
         </div>
+        <CartSidebar isOpen={isCartOpen} onClose={toggleCart} />
       </div>
     </nav>
   );
