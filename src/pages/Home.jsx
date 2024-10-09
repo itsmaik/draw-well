f7b7124c39c8c686788755f18b7ce646fb9e50ca
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-09 14:11:59 +0200

    Adds min-h-screen to front page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import DisplayAll from "../components/DisplayAll";
-import { useEffect } from "react";
 
 function Home() {
 
   return (
-    <main>
+    <main className="flex flex-col min-h-screen">
       <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
         <DisplayAll />
       </section>
