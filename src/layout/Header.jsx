1eac26b07b1036615606a4e7dbe8afb79104cc7f
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-09 23:26:42 +0200

    Transfers hero img from header to home

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,10 @@
-import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import HeroImg from "../components/HeroImg";
 
 export default function Header() {
-  const location = useLocation();
 
   return (
     <header>
       <Navbar />
-      {/* Render HeroImg only if the current path is the homepage */}
-      {location.pathname === "/" && <HeroImg />}
     </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import DisplayAll from "../components/DisplayAll";
+import HeroImg from "../components/HeroImg";
 
 function Home() {
 
   return (
     <main className="flex flex-col min-h-screen">
+      <HeroImg />
       <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
         <DisplayAll />
       </section>
