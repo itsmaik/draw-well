2ff2819e559b372586f2a496912cc87f69f1ab02
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-09 13:41:36 +0200

    Removes function that makes page scroll to the top on reload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,6 @@ import { useEffect } from "react";
 
 function Home() {
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <main>
       <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
