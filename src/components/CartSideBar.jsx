cc14c80a53710e624ca0fe02da3d63b82ee2feab
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-10 14:10:28 +0200

    Adds logic to close sideBar on page change

--- src/components/CartSideBar.jsx
+++ src/components/CartSideBar.jsx
@@ -1,20 +1,33 @@
 import { FaTimes, FaTrash } from "react-icons/fa";
 import useCartCalculations from "../hooks/useCartCalculations";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
-export default function CartSidebar({ isOpen, onClose }) {
+export default function CartSidebar({ isOpen, onClose, }) {
   const { cartItems, removeFromCart, totalPrice, totalDiscount, totalToPay, handleIncreaseQuantity,handleDecreaseQuantity, } = useCartCalculations();
 
   const navigate = useNavigate();
+  const location = useLocation();
+  const prevLocation = useRef(location);
+  
+  useEffect(() => {
+    if (isOpen && prevLocation.current !== location) {
+      onClose();
+    }
+    prevLocation.current = location;
+  }, [location, isOpen, onClose]);
+
+
 
   const handleCheckout = () => {
     navigate("/checkout");
+    onClose();
   };
 
 
   return (
     <div
-      className={`fixed top-0 right-0 h-full bg-white shadow-lg transition-transform transform ${
+      className={`fixed top-0 right-0 h-full bg-white shadow-lg transform transition-transform duration-500 ease-in-out ${
         isOpen ? "translate-x-0" : "translate-x-full"
       } w-80 z-50 flex flex-col`}
     >
@@ -28,7 +41,7 @@ export default function CartSidebar({ isOpen, onClose }) {
       {/* Scrollable Cart Items */}
       <div className="flex-grow overflow-y-auto p-4">
         {cartItems.length === 0 ? (
-          <p>Your cart is empty.</p>
+          <p className="text-red-500 text-lg">Your cart is empty.</p>
         ) : (
           cartItems.map((item) => (
             <div key={item.id} className="flex justify-between items-center mb-4">
@@ -86,7 +99,7 @@ export default function CartSidebar({ isOpen, onClose }) {
           <p>${totalToPay.toFixed(2)}</p>
         </div>
 
-        <button className="bg-orange-500 text-white w-full py-2 rounded mt-4 text-xl font-semibold" onClick={handleCheckout}>
+        <button className={`w-full py-2 rounded mt-4 text-xl font-semibold ${cartItems.length=== 0 ? "bg-gray-300 cursor-default" : "bg-orange-500 text-white"}`} onClick={handleCheckout} disabled={cartItems.length === 0}>
           Go to Checkout
         </button>
       </div>
