b88321cd192027de90105e4798c71576b3ee16c8
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-10 14:11:15 +0200

    Adds logic to remove HeroImg while loading or error handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import DisplayAll from "../components/DisplayAll";
 import HeroImg from "../components/HeroImg";
+import { useContext } from "react";
+import { ProductContext } from "../context/ProductProvider";
 
 function Home() {
+  const { searchQuery, loading, error } = useContext(ProductContext);
 
   return (
     <main className="flex flex-col min-h-screen">
-      <HeroImg />
-      <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
+      {!searchQuery && !error && !loading && <HeroImg />}
+      <section className="container mx-auto">
         <DisplayAll />
       </section>
     </main>
