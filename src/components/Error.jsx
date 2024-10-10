bb9372a8f0a0fd83668dfdbb51407178922388c1
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-10 14:11:50 +0200

    Updates css for error and loader components

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,6 @@
 export default function Error ({error}) {
   return(
-    <div className="flex justify-center items-center min-h-screen fixed top-0 z-50 min-w-full bg-slate-50">
+    <div className="flex justify-center items-center min-h-screen min-w-full bg-slate-50">
       <p className="text-3xl font-semibold text-red-500">{error}</p>
     </div>
   )
