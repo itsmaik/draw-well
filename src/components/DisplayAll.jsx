8a312c017d0e756bc606b91ac12e1d602e7cb94f
Maik Helland Olsen <126066270+itsmaik@users.noreply.github.com>
2024-10-10 14:15:35 +0200

    Refactors  code structure but keeps same logic

--- src/components/DisplayAll.jsx
+++ src/components/DisplayAll.jsx
@@ -11,17 +11,19 @@ export default function DisplayAll() {
   if (error) return <Error error={error} />
   if (searchResults === undefined) return;
 
+  if (searchResults.length === 0) {
+    return (
+      <div className="flex justify-center items-center min-h-80 fixed top-0 min-w-full bg-slate-50">
+        <p>No product found</p>
+      </div>
+    );
+  }
+
   return (
-    <>
-      {searchResults && searchResults.length ? (
-        searchResults.map((product) => (
-          <ProductCard products={product} key={product.id} />
-        ))
-      ) : (
-        <div className="flex justify-center items-center min-h-80 fixed top-0 min-w-full bg-slate-50">
-          <p>No product found</p>
-        </div>
-      )}
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
+      {searchResults.map((product) => (
+        <ProductCard products={product} key={product.id} />
+      ))}
+    </div>
   );
 }
